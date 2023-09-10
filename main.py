452e3134e601cae0b7a89a320c4b925fe6e3e2a0
FeGonca <felip.goncalves@hotmail.com>
2023-09-10 15:29:43 -0300

    Adicionando as tratamento e insercao

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import requests
+import pandas as pd
 import json
 import config
 import api_filmes
+from database.conexao_engine import conexao_engine
 
 def carrega_filmes():
     # Carregando os filmes da API do site omdbapi.com
     lista_filmes_api = []
 
     # ler arquivo com a lista de titulos de filmes    
-    with open('data/amostra_filmes.txt', 'r') as arquivo_filmes:
+    with open('data/filmes.txt', 'r') as arquivo_filmes:
         filmes = arquivo_filmes.readlines()
         arquivo_filmes.close()
 
@@ -35,6 +37,7 @@ def salva_arquivo():
     print('Arquivo gerado com sucesso!')
 
 def trata_json():
+    df_consolida = []
     # Tratar o conteúdo do retorno da API
 
     # Recebe lista e arquivos
@@ -43,7 +46,38 @@ def trata_json():
     # Verificando se a TAG Ratings possui valor ou objeto e realizando o devido tratamento
     for filme_api in lista_filmes_api:
         i = 0
+        df_ratings = []
+
         if isinstance(filme_api['Ratings'], list):
             # Passados os objetos
             for df_ratings in filme_api['Ratings']:
-                # continuar
+                tag_source = 'Rating_Source_' + str(i)
+                tag_value = 'Rating_Value_' + str(i)
+                filme_api[tag_source] = df_ratings["Source"]
+                filme_api[tag_value] = df_ratings["Value"]
+                i += 1
+        else:
+            filme_api['Rating_Source_0'] = filme_api['Source']
+            filme_api['Rating_Value_0'] = filme_api['Value']
+
+        filme_api.pop('Ratings')
+        df_consolida.append(filme_api)
+    
+    pd.set_option('display.max_columns', None)
+    df_filme = pd.DataFrame(df_consolida)
+    df_filme.columns = df_filme.columns.str.lower()
+
+    return df_filme
+
+def inseri_filmes():
+    
+    # Recebi o DataFrame filme
+    df_filme = trata_json()
+
+    # Conexao com Banco de Dados
+    engine = conexao_engine()
+
+    # Insert, 'append' = adiciona, 'replace' = apaga dos dados da tabela e coloca os dados do datafram, 'fail' = erro se a tabela estiver criada
+    df_filme.to_sql('filmes', engine, if_exists='replace', index=False)
+
+inseri_filmes()
--- testes_db.py
+++ testes_db.py
@@ -6,14 +6,16 @@ from database.DML.delete import delete_table
 from database.DML.update import update_table
 
 
-cria_tabela('MINHA_TABLE')
-consulta('MINHA_TABLE')
+# cria_tabela('MINHA_TABLE')
+# consulta('MINHA_TABLE')
 
-insert_table('MINHA_TABLE', 1, 'Felipe', 'felipe@email.com')
-consulta('MINHA_TABLE')
+# insert_table('MINHA_TABLE', 1, 'Felipe', 'felipe@email.com')
+# consulta('MINHA_TABLE')
 
-update_table('MINHA_TABLE', 1, 'Felipe', 'felipe@hotmail.com')
-consulta('MINHA_TABLE')
+# update_table('MINHA_TABLE', 1, 'Felipe', 'felipe@hotmail.com')
+# consulta('MINHA_TABLE')
 
-delete_table('MINHA_TABLE', 1)
-consulta('MINHA_TABLE')
+# delete_table('MINHA_TABLE', 1)
+# consulta('MINHA_TABLE')
+
+consulta('filmes')
