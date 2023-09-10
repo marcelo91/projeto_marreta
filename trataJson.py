64dfc672aa892679b1eb4a9f70274e1633568b2d
FeGonca <felip.goncalves@hotmail.com>
2023-09-10 15:42:19 -0300

    Adicionados ao gitignore

--- trataJson.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import json
- 
-with open("data/movie_data.json") as myFile:
-  fileData = json.load(myFile)
-  dataFrame = pd.DataFrame(columns=fileData[0].keys())
-  for i in range(len(fileData)):
-     dataFrame.loc[i] = fileData[i].values()
-     print(dataFrame)
